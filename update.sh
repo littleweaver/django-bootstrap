f258c2c7be15db4f0e50e85d5d68ac3a17be44ff
Harris Lapiroff <harris@chromamine.com>
2015-10-27 14:15:50 -0400

    update script doesn't work; let's just do it manually

--- update.sh
+++
@@ -1,6 +0,0 @@
-BOOTSTRAP_REPO="https://github.com/twbs/bootstrap.git"
-if ! git ls-remote bootstrap_remote &> /dev/null ; then
-	git remote add bootstrap_remote $BOOTSTRAP_REPO
-fi
-git fetch bootstrap_remote
-git read-tree --prefix=bootstrap/static/bootstrap/ -u $1:scss/
