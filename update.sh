e9d261a51e48b899c8ed0fc8fc52da92efbfd07a
Harris Lapiroff <harris@chromamine.com>
2015-10-27 14:06:33 -0400

    Added bootstrap

--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@ if ! git ls-remote bootstrap_remote &> /dev/null ; then
 	git remote add bootstrap_remote $BOOTSTRAP_REPO
 fi
 git fetch bootstrap_remote
-git read-tree --prefix=bootstrap/static/bootstrap/ -um $1:scss/
+git read-tree --prefix=bootstrap/static/bootstrap/ -u $1:scss/
